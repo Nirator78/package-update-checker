bb64f55fbc0d742317d57cf546601795c868e456
Nirator78 <clementduval786@gmail.com>
2024-05-12 21:42:11 +0200

    Add validation schema for config file

---
+++ src/schemas/config-file.schema.ts
@@ -0,0 +1,46 @@
+import { RepositoryType } from "../enums/repository-type.enum";
+import { PackageType } from "../enums/package-type.enum";
+import { AlertType } from "../enums/alert-type.enum";
+import { AlertSource } from "../enums/alert-source.enum";
+
+export const configFileSchema = {
+  type: "array",
+  minItems: 1,
+  items: {
+    type: "object",
+    properties: {
+      name: { type: "string" },
+      type: { type: "string", enum: Object.values(RepositoryType) },
+      url: { type: "string" },
+      description: { type: "string" },
+      private: { type: "boolean" },
+      fileToCheck: {
+        type: "array",
+        items: {
+          type: "object",
+          properties: {
+            path: { type: "string" },
+            type: { type: "string", enum: Object.values(PackageType) },
+          },
+          required: ["path", "type"]
+        }
+      },
+      alerts: {
+        type: "array",
+        items: {
+          type: "object",
+          properties: {
+            type: { type: "string", enum: Object.values(AlertType) },
+            source: { type: "string", enum: Object.values(AlertSource) },
+            url: { type: "string" },
+            title: { type: "string" },
+            onlyIfUpdateNeeded: { type: "boolean" }
+          },
+          required: ["type", "source", "url"]
+        },
+        minItems: 1
+      }
+    },
+    required: ["name", "type", "url", "fileToCheck", "alerts"]
+  }
+};
