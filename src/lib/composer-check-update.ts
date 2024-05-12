bb9cdcb2bfc9e040662ebcd525f430d515c6f3ea
Nirator78 <clementduval786@gmail.com>
2024-05-12 15:42:08 +0200

    Add the first version of the composer json check update

---
+++ src/lib/composer-check-update.ts
@@ -0,0 +1,54 @@
+import axios from "axios";
+
+import { Package } from "../interfaces/package.interface"
+
+export const composerCheckUpdate = async (composerJsonContent) => {
+    const table: Package[] = [];
+
+    const packageTypes = ["require", "require-dev"];
+
+    for (const packageType of packageTypes) {
+      const pkgs = composerJsonContent[packageType];
+
+      for (const [package_, version] of Object.entries(pkgs)) {
+          if (package_ === "php") {
+            continue;
+          }
+
+          if (package_.startsWith("ext-")) {
+            continue;
+          }
+
+          try {
+            const response = await axios.get(`https://repo.packagist.org/p2/${package_}.json`);
+
+            if (response.status >= 400) {
+              throw new Error("Bad response from server");
+            }
+
+            const body = response.data.packages[package_][0];
+            
+            if(body.version.replace(/[^0-9.]/g, '') !== (version as string).replace(/[^0-9.]/g, ''))
+              table.push({
+                package: package_,
+                version: body.version.replace(/[^0-9.]/g, ''),
+                current: (version as string).replace(/[^0-9.]/g, ''),
+                url: `https://packagist.org/packages/${package_}`
+              })
+          } catch(e) {
+            console.log(
+              `Skipped ${package_} because it encountered an error`,
+            );
+            console.log(e)
+            table.push({
+              package: package_,
+              version: "Error",
+              current: version as string,
+              url: `https://packagist.org/packages/${package_}`
+            })
+          }
+      }
+    }
+
+    return table;
+};
