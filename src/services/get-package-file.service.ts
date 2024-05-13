323f8018b07bf95884deec755d0c51acc8b5408b
Nirator78 <clementduval786@gmail.com>
2024-05-13 21:04:00 +0200

    Add get file service

---
+++ src/services/get-package-file.service.ts
@@ -0,0 +1,16 @@
+import { RepositoryType } from "../enums/repository-type.enum";
+import { IRepository, IFileToCheck } from "../interfaces/repository.interface";
+import { getPackageFromGithubService } from "./get-package-file/get-package-from-github.service";
+import { getPackageFromTortoiseSvnService } from "./get-package-file/get-package-from-tortoisesvn.service";
+
+export const getPackageFileService = async (repository: IRepository, fileToCheck: IFileToCheck): Promise<any> => {
+	const { type } = repository;
+	switch (type) {
+		case RepositoryType.GITHUB:
+			return getPackageFromGithubService(repository, fileToCheck);
+		case RepositoryType.TORTOISESVN:
+			return getPackageFromTortoiseSvnService(repository, fileToCheck);
+		default:
+			throw new Error('Repository type not supported');
+	}
+};
