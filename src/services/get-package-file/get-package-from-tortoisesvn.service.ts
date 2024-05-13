2ed83f7c86edc10e053161f12401ae5b8896eed2
Nirator78 <clementduval786@gmail.com>
2024-05-13 21:03:43 +0200

    Add get file from tortoisesvn

---
+++ src/services/get-package-file/get-package-from-tortoisesvn.service.ts
@@ -0,0 +1,4 @@
+import { IFileToCheck, IRepository } from "../../interfaces/repository.interface";
+
+export const getPackageFromTortoiseSvnService = async (repository: IRepository, fileToCheck: IFileToCheck): Promise<any> => {
+};
