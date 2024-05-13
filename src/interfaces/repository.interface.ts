9a2dd102fc9aaa1443f91993a035c61667dc4eba
Nirator78 <clementduval786@gmail.com>
2024-05-13 20:21:12 +0200

    Add repository interface

---
+++ src/interfaces/repository.interface.ts
@@ -0,0 +1,22 @@
+interface IFileToCheck {
+	path: string;
+	type: string;
+  }
+  
+interface IAlert {
+	type: string;
+	source: string;
+	url: string;
+	title: string;
+	onlyIfUpdateNeeded: boolean;
+  }
+  
+export interface IRepository {
+	name: string;
+	type: string;
+	url: string;
+	description: string;
+	private: boolean;
+	fileToCheck: IFileToCheck[];
+	alerts: IAlert[];
+  }
