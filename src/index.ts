e763de7480f7751aa7dbef7532997ff3b7d3406c
Nirator78 <clementduval786@gmail.com>
2024-05-13 21:04:14 +0200

    Implement get file service and npm / composer check update

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,30 @@
 // Read config file
 const config = require('../config.json');
+// Import utilities
+import { verifyConfigFileService } from './services/verify-config-file.service';
+import { getPackageFileService } from './services/get-package-file.service';
+import { npmCheckUpdate } from './lib/npm-check-update';
+import { composerCheckUpdate } from './lib/composer-check-update';
+// Import Enums
+import { PackageType } from './enums/package-type.enum';
 
 // Validate config file
-import { verifyConfigFileService } from './services/verify-config-file.service';
 verifyConfigFileService(config);
 
-// For each repository do the work 💪💪💪
-for (const repository of config) {
-  // Get the files to check
-  // Check if the files are up to date
-  // Send alerts if needed
-}
+(async () => {
+    // For each repository do the work 💪💪💪
+	for (const repository of config) {
+		for (const fileToCheck of repository.fileToCheck) {
+			// Get the files to check
+			const packageContent = await getPackageFileService(repository, fileToCheck);
+			// Check if the files are up to date
+			let update;
+			if (fileToCheck.type === PackageType.NPM) {
+				update = await npmCheckUpdate(packageContent);
+			} else {
+				update = await composerCheckUpdate(packageContent);
+			}
+			// Send alerts if needed
+		}
+	}
+})();
