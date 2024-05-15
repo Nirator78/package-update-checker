3c2d6499ce82e5481ce54db570a76f3261159538
Nirator78 <nirator.dev@gmail.com>
2024-05-15 17:10:35 +0200

    Add get package from tortoise svn

--- src/services/get-package-file/get-package-from-tortoisesvn.service.ts
+++ src/services/get-package-file/get-package-from-tortoisesvn.service.ts
@@ -1,4 +1,28 @@
+import axios from "axios";
+import * as https from "https";
 import { IFileToCheck, IRepository } from "../../interfaces/repository.interface";
 
+const httpsAgent = new https.Agent({ rejectUnauthorized: false });
+
+let config = {
+  method: "get",
+  maxBodyLength: Infinity,
+  httpsAgent,
+  url: "",
+  headers: { 
+    "Authorization": ""
+  },
+};
+
 export const getPackageFromTortoiseSvnService = async (repository: IRepository, fileToCheck: IFileToCheck): Promise<any> => {
+	if(repository.private) {
+        config.url = `${repository.url}/${fileToCheck.path}`;
+        config.headers.Authorization = `Basic ${repository.auth}`;
+		const response = await axios.request(config);
+        return response.data;
+	} else {
+        config.url = `${repository.url}/${fileToCheck.path}`;
+		const response = await axios.request(config);
+		return response.data;
+	}
 };
