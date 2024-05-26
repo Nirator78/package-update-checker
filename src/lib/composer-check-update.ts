e78214fdceaec2fa47036d8b86318413d00e0fe3
Nirator78 <nirator.dev@gmail.com>
2024-05-26 19:17:27 +0200

    Indentation

--- src/lib/composer-check-update.ts
+++ src/lib/composer-check-update.ts
@@ -11,48 +11,48 @@ export const composerCheckUpdate = async (composerJsonContent): Promise<IPackage
     const packageTypes = ["require", "require-dev"];
 
     for (const packageType of packageTypes) {
-      const pkgs = composerJsonContent[packageType];
-      if(!pkgs) continue;
+		const pkgs = composerJsonContent[packageType];
+		if(!pkgs) continue;
 
-      for (let [packageName, version] of Object.entries(pkgs)) {
-          if (packageName === "php") {
-            continue;
-          }
+		for (let [packageName, version] of Object.entries(pkgs)) {
+			if (packageName === "php") {
+				continue;
+			}
 
-          if (packageName.startsWith("ext-")) {
-            continue;
-          }
+			if (packageName.startsWith("ext-")) {
+				continue;
+			}
 
-          try {
-            const response = await axios.get(`https://repo.packagist.org/p2/${packageName}.json`);
+			try {
+				const response = await axios.get(`https://repo.packagist.org/p2/${packageName}.json`);
 
-            if (response.status >= 400) {
-              throw new Error("Bad response from server");
-            }
+				if (response.status >= 400) {
+				throw new Error("Bad response from server");
+				}
 
-			const body = response.data.packages[packageName][0];
-			const lastestVersion = body.version;
-			version = (version as string).replace(/[^0-9.]/g, "");
+				const body = response.data.packages[packageName][0];
+				const lastestVersion = body.version;
+				version = (version as string).replace(/[^0-9.]/g, "");
 
-            if(semverLt(version, lastestVersion))
-              table.push({
-                package: packageName,
-                version: lastestVersion,
-                current: version as string,
-                url: `https://packagist.org/packages/${packageName}`,
-				releaseType: semverDiff(version, lastestVersion)
-              })
-          } catch(e) {
-			console.error(e);
-            table.push({
-              	package: packageName,
-              	version: "Error",
-              	current: version as string,
-              	url: `https://packagist.org/packages/${packageName}`,
-			  	releaseType: ReleaseType.ERROR
-            })
-          }
-      }
+				if(semverLt(version, lastestVersion))
+				table.push({
+					package: packageName,
+					version: lastestVersion,
+					current: version as string,
+					url: `https://packagist.org/packages/${packageName}`,
+					releaseType: semverDiff(version, lastestVersion)
+				})
+			} catch(e) {
+				console.error(e);
+				table.push({
+					package: packageName,
+					version: "Error",
+					current: version as string,
+					url: `https://packagist.org/packages/${packageName}`,
+					releaseType: ReleaseType.ERROR
+				})
+			}
+		}
     }
 
     return table;
