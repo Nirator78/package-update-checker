f12d0fbddeee5810f68dc121f3946a71b6f6618c
Nirator78 <nirator.dev@gmail.com>
2024-06-04 20:25:58 +0200

    Add private gitlab manage

--- src/services/get-package-file/get-package-from-gitlab.service.ts
+++ src/services/get-package-file/get-package-from-gitlab.service.ts
@@ -3,7 +3,16 @@ import { IFileToCheck, IRepository } from "@/interfaces/repository.interface";
 
 export const getPackageFromGitlabService = async (repository: IRepository, fileToCheck: IFileToCheck): Promise<any> => {
 	if(repository.private) {
-		throw new Error("Private Gitlab repositories are not supported yet");
+		const url = new URL(repository.url);
+		const domain = url.hostname;
+		const repositoryName = repository.url.replace(`https://${domain}/`, "").split("/")[1];
+		const projectListUrl = `https://${domain}/api/v4/projects?private_token=${repository.auth}&search=${repositoryName}&visibility=private`;
+		const projectListResponse = await axios.get(projectListUrl);
+		
+		const projectId = projectListResponse.data[0].id;
+		const rawGithubFile = `https://${domain}/api/v4/projects/${projectId}/repository/files/${fileToCheck.path}/raw?private_token=${repository.auth}`;
+		const response = await axios.get(rawGithubFile);
+        return response.data;
 	} else {
 		const url = `${repository.url}/-/raw/main/${fileToCheck.path}`;
 		const response = await axios.get(url);
